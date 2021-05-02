63b2880fa1e9a0305142c35836b8a71778a9e359
Matteo Medioli <m.medioli@studenti.unipi.it>
2021-05-02 19:46:07 +0200

    renamed run_graph_sage to _conv

--- graph_model.sh
+++ graph_model.sh
@@ -1,4 +1,4 @@
 source /data/medioli/env/bin/activate
 export CUDA_VISIBLE_DEVICES=0 
 export OMP_NUM_THREADS=128
-nohup python3 -m torch.distributed.launch --nproc_per_node 1 run_irgnn.py &
+nohup python3 -m torch.distributed.launch --nproc_per_node 1 run_graph_conv.py &
